6c7cf980c325fe7b0a62cef38b4816f5bbaf53ac
luisdev97 <amadormontorodavid@gmail.com>
2019-12-08 15:44:22 +0100

    Budget validated

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+ 
 const Question = () => {
+
+    const [ quantity, setQuantity ] = useState(0);
+    const [ error, setError ] = useState(false);
+
+    const handleChange = e => {
+        setQuantity(parseInt(e.target.value));
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(quantity < 1 || isNaN(quantity)){
+            setError(true);
+        }else{
+            setError(false);
+        }
+    }
+
     return (
         <>
             <h2>Budget</h2>
-            <form>
+
+            { error && <p className="alert alert-danger error">The budget is incorrect</p> }
+
+            <form onSubmit={ handleSubmit }>
                 <input type="number"
                 className="u-full-width"
-                placeholder="add your budget"/>
+                placeholder="add your budget"
+                onChange={ handleChange }
+                value={ quantity}/>
                 <button type="submit" className="button-primary u-full-width">Define budget</button>
             </form>
         </>
